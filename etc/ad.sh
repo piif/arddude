93b6c73121dd9f9e152f63c48768d4cc7e739499
piif <christian_lefebvre@laposte.net>
2014-03-22 16:41:19 +0100

    shell to launch jar

--- etc/ad.sh
+++ etc/ad.sh
@@ -6,20 +6,11 @@ ROOTDIR="$(dirname "$HERE")"
 CLASSPATH="$(grep "lib" $ROOTDIR/.classpath |cut -d '"' -f 4|tr '\n' :)$ROOTDIR/bin"
 
 if expr $(uname) : CYGWIN > /dev/null ; then
-	convPath() {
-		echo "$1" | tr ":" "\n" | while read l ; do
-			cygpath -w "$l"
-		done | tr "\n" ";"
-	}
-	CLASSPATH="$(convPath "$CLASSPATH")"
-	
 	JAVA="$(/bin/ls -d1 /cygdrive/c/Program*/Java/*/bin/java | tail -1)"
 else
 	JAVA=java
 fi
 
 JAR="$(/bin/ls -d1 $ROOTDIR/target/ArdDude-*.jar | tail -1)"
-opt="-Djava.library.path=/usr/lib64/rxtx"
 
-##"$JAVA" -cp "$CLASSPATH" $opt -jar "$JAR" "$@"
-"$JAVA" -cp "$CLASSPATH" $opt pif.arduino.ArdDude "$@"
+exec "$JAVA" -jar "$JAR" "$@"
