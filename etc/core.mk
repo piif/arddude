45d0e03f85a3cb8479ee5bd784389ae20a525071
piif <christian.lefebvre@worldline.com>
2015-03-24 16:33:09 +0100

    compile system/libsam with core

--- etc/core.mk
+++ etc/core.mk
@@ -10,6 +10,10 @@ TARGET_DIR := ${ARDDUDE_DIR}/target/${TARGET_BOARD}
 include ${ARDDUDE_DIR}/etc/common.mk
 
 SOURCE_DIRS := ${CORE_DIR} ${VARIANT_DIR}
+ifneq ($(findstring arduino_due,${TARGET_BOARD}),)
+SOURCE_DIRS += ${ARDUINO_IDE}/hardware/arduino/sam/system/libsam
+CFLAGS_EXTRA := -std=c11 -I${ARDUINO_IDE}/hardware/arduino/sam/system/libsam/include
+endif
 ALL_SOURCES := $(foreach d,${SOURCE_DIRS},$(call rwildcard,$d,*.c) $(call rwildcard,$d,*.cpp))
 OBJS := $(addsuffix .o,$(basename ${ALL_SOURCES:${HARDWARE_DIR}/%=${TARGET_DIR}/%}))
 
