d4a6c240b00d75b303d053ca95bb535c3d55ffbb
piif <christian_lefebvre@laposte.net>
2019-09-24 23:37:48 +0200

    cleanup

--- etc/ArduinoISP.sh
+++ etc/ArduinoISP.sh
@@ -2,5 +2,7 @@
 
 ICI=$(dirname $0)
 
-$ARDUINO_IDE/hardware/tools/avr/bin/avrdude -C$ARDUINO_IDE/hardware/tools/avr/etc/avrdude.conf -v -patmega328p -carduino -P/dev/ttyACM0 -b115200 -D -Uflash:w:$ICI/ArduinoISP.cpp.hex:i 
-
+$ARDUINO_IDE/hardware/tools/avr/bin/avrdude \
+	-C$ARDUINO_IDE/hardware/tools/avr/etc/avrdude.conf \
+	-v -patmega328p -carduino -P/dev/ttyACM0 -b115200 \
+	-D -Uflash:w:$ICI/ArduinoISP.cpp.hex:i 
