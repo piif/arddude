c1c5c830500983aea8abd79ce24d5e9f05493175
Christian Lefebvre <christian_lefebvre@laposte.net>
2022-07-21 15:39:57 +0200

    fix default usb port name

--- etc/ArduinoISP.sh
+++ etc/ArduinoISP.sh
@@ -4,5 +4,5 @@ ICI=$(dirname $0)
 
 $ARDUINO_IDE/hardware/tools/avr/bin/avrdude \
 	-C$ARDUINO_IDE/hardware/tools/avr/etc/avrdude.conf \
-	-v -patmega328p -carduino -P/dev/ttyACM0 -b115200 \
+	-v -patmega328p -carduino -P/dev/ttyUSB0 -b115200 \
 	-D -Uflash:w:$ICI/ArduinoISP.cpp.hex:i 
