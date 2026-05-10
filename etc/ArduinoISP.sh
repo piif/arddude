223ae8c5ad80baf7aad237a49f39584f6ca52da8
Christian Lefebvre <christian_lefebvre@laposte.net>
2026-05-10 22:59:35 +0200

    allow additionnal arguments to ArduinoISP command

--- etc/ArduinoISP.sh
+++ etc/ArduinoISP.sh
@@ -5,4 +5,4 @@
 HERE=$(cd $(dirname $0) ; /bin/pwd)
 ARDDUDE=$(dirname $HERE)
 
-make -f $ARDDUDE/etc/Makefile -C $ARDDUDE/src/arduino/ArduinoISP upload
+make -f $ARDDUDE/etc/Makefile -C $ARDDUDE/src/arduino/ArduinoISP "$@" upload
